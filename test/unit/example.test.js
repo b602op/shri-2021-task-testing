acfff1e7d8d2f35ac71082b2529eb6fcb97401cd
Eduard Ispolatov <u281094@gmail.com>
2021-10-25 23:26:48 +0300

    #yandex new test

--- test/unit/example.test.js
+++ test/unit/example.test.js
@@ -1,11 +1,12 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
-
-import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
 
-import { Application } from "../../src/client/Application"
-import { ExampleApi, CartApi } from "../../src/client/api"
+import "@testing-library/jest-dom/extend-expect"
+import { render } from "@testing-library/react"
+
+import { Catalog } from "../../src/client/pages/Catalog"
+import { CartApi, ExampleApi } from "../../src/client/api"
 import { initStore } from "../../src/client/store"
 
 const extraUrl = "/hw/store";
@@ -13,60 +14,18 @@ const API_URL = new ExampleApi(extraUrl);
 const CART_API = new CartApi();
 const currentStore = initStore(API_URL, CART_API);
 
-describe("Check for page-links and cart to be in navbar", () => {
-  it("Should find cart and at least one page link", () => {
-    const { container } = render(
+describe("Каталог здесь? Спартак здесь?", () => {
+  it("Каталооооооооооооооог", async () => {
+    const { getByText } = await render(
       <BrowserRouter basename={extraUrl}>
         <Provider store={currentStore}>
-          <Application />
+          <Catalog />
         </Provider>
-      </BrowserRouter>
+    </BrowserRouter>
     )
 
-    const navLinkSelector = ".nav-link"
+    const textFirst = getByText('Catalog');
 
-    const navPageLinks = container.querySelectorAll(navLinkSelector)
-    const navCart = container.querySelectorAll(
-      `${navLinkSelector}[href="${extraUrl}/cart"]`
-    )
-
-    expect(navPageLinks.length).toBeGreaterThan(1)
-    expect(navCart.length).toBe(1)
-  })
-
-  it("проверка ссылки у navbar-brand", () => {
-    const { container } = render(
-      <BrowserRouter basename={extraUrl}>
-        <Provider store={currentStore}>
-          <Application />
-        </Provider>
-      </BrowserRouter>
-    )
-
-    const shopTitleSelector = ".navbar-brand"
-
-    expect(
-      container.querySelector(shopTitleSelector).getAttribute("href")
-    ).toBe(extraUrl + "/")
-  })
-
-  it("Проверка ссылок в Application", () => {
-    render(
-      <BrowserRouter basename={extraUrl}>
-        <Provider store={currentStore}>
-          <Application />
-        </Provider>
-      </BrowserRouter>
-    )
-
-    const homeLink = screen.queryByText("Example store")
-    const catalogLink = screen.queryByText("Catalog")
-    const deliveryLink = screen.queryByText("Delivery")
-    const contactsLink = screen.queryByText("Contacts")
-
-    expect(homeLink).toBeInTheDocument()
-    expect(catalogLink).toBeInTheDocument()
-    expect(deliveryLink).toBeInTheDocument()
-    expect(contactsLink).toBeInTheDocument()
+    expect(textFirst).toBeInTheDocument()
   })
 })
